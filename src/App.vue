cf609f59216809a3301ffd5181ea6ce51b7654ee
Ronei Carlos <41250984+RoneiCarlos@users.noreply.github.com>
2021-02-21 22:30:51 -0300

    navbar work

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <div id="app">
-    <router-view />
+    <div class="container grid-lg">
+      <header class="navbar">
+        <section class="navbar-section"></section>
+        <section class="navbar-center py-2">
+          <router-link to="/" class="btn btn-link">Home</router-link>
+          <router-link to="/kanban" class="btn btn-link">Kanban</router-link>
+        </section>
+        <section class="navbar-section"></section>
+      </header>
+      <router-view />
+    </div>
   </div>
 </template>
